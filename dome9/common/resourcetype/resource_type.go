47bbc7b23d248f7c5c6aca4765ec22d3622dd46a
Erez Weinstein <erez@dome9.com>
2019-12-03 14:54:37 +0200

    Ruleset resource renaming

--- dome9/common/resourcetype/resource_type.go
+++ dome9/common/resourcetype/resource_type.go
@@ -8,5 +8,5 @@ const (
 	IPList                           = "dome9_iplist"
 	ContinuousCompliancePolicy       = "dome9_continuous_compliance_policy"
 	ContinuousComplianceNotification = "dome9_continuous_compliance_notification"
-	RuleSet                          = "dome9_rule_set"
+	RuleSet                          = "dome9_ruleset"
 )
