80ca9ab9516c330a98c324d6d4e66ddd587b5d4e
Erez Weinstein <erez@dome9.com>
2019-12-18 10:27:27 +0200

    CR Fix

--- examples/attach_iam_safe/main.tf
+++ examples/attach_iam_safe/main.tf
@@ -3,23 +3,3 @@ resource "dome9_attach_iam_safe" "dome9_attach_iam_safe_re" {
   aws_group_arn        = "AWS_GROUP_ARN"
   aws_policy_arn       = "AWS_POLICY_ARN"
 }
-
-data "dome9_attach_iam_safe" "dome9_attach_iam_safe_ds" {
-  id = "00000000-0000-0000-0000-000000000000"
-}
-
-output "aws_cloud_account_id" {
-  value = "${data.dome9_attach_iam_safe.dome9_attach_iam_safe_ds.aws_cloud_account_id}"
-}
-
-output "aws_group_arn" {
-  value = "${data.dome9_attach_iam_safe.dome9_attach_iam_safe_ds.aws_group_arn}"
-}
-
-output "aws_policy_arn" {
-  value = "${data.dome9_attach_iam_safe.dome9_attach_iam_safe_ds.aws_policy_arn}"
-}
-
-output "mode" {
-  value = "${data.dome9_attach_iam_safe.dome9_attach_iam_safe_ds.mode}"
-}
