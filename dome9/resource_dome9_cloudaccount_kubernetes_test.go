462a8cede2888a51fe22fd7fc76019e3b5bef942
Omer Zentner <omerze+github@checkpoint.com>
2021-06-09 11:56:49 +0300

    Add IA, AC, RP

--- dome9/resource_dome9_cloudaccount_kubernetes_test.go
+++ dome9/resource_dome9_cloudaccount_kubernetes_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dome9/dome9-sdk-go/services/cloudaccounts/k8s"
 	"github.com/terraform-providers/terraform-provider-dome9/dome9/common/testing/environmentvariable"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -18,6 +19,7 @@ import (
 func TestAccResourceCloudAccountKubernetesBasic(t *testing.T) {
 	var cloudAccountResponse k8s.CloudAccountResponse
 	resourceTypeAndName, _, generatedName := method.GenerateRandomSourcesTypeAndName(resourcetype.CloudAccountKubernetes)
+	anotherResourceTypeAndName, _, anotherGeneratedName := method.GenerateRandomSourcesTypeAndName(resourcetype.CloudAccountKubernetes)
 	defaultOrganizationalUnitName := os.Getenv(environmentvariable.OrganizationalUnitName)
 	organizationUnitTypeAndName, _, organizationUnitGeneratedName := method.GenerateRandomSourcesTypeAndName(resourcetype.OrganizationalUnit)
 	organizationUnitHCL := getOrganizationalUnitResourceHCL(organizationUnitGeneratedName, variable.OrganizationalUnitName)
@@ -39,6 +41,9 @@ func TestAccResourceCloudAccountKubernetesBasic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceTypeAndName, "creation_date"),
 					resource.TestCheckResourceAttr(resourceTypeAndName, "vendor", variable.CloudAccountKubernetesVendor),
 					resource.TestCheckResourceAttr(resourceTypeAndName, "organizational_unit_name", defaultOrganizationalUnitName),
+					resource.TestCheckResourceAttrSet(resourceTypeAndName, "runtime_protection.0.enabled"),
+					resource.TestCheckResourceAttrSet(resourceTypeAndName, "admission_control.0.enabled"),
+					resource.TestCheckResourceAttrSet(resourceTypeAndName, "image_assurance.0.enabled"),
 				),
 			},
 			{
@@ -62,6 +67,34 @@ func TestAccResourceCloudAccountKubernetesBasic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceTypeAndName, "vendor", variable.CloudAccountKubernetesVendor),
 					resource.TestCheckResourceAttrPair(resourceTypeAndName, "organizational_unit_id", organizationUnitTypeAndName, "id"),
 					resource.TestCheckResourceAttrPair(resourceTypeAndName, "organizational_unit_name", organizationUnitTypeAndName, "name"),
+				),
+			},
+			{
+				// Create with features
+				Config: testAccCheckCloudAccountKubernetesCreateWithFeatures(anotherResourceTypeAndName, anotherGeneratedName, variable.CloudAccountKubernetesOriginalAccountName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudAccountKubernetesExists(anotherResourceTypeAndName, &cloudAccountResponse),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "name", variable.CloudAccountKubernetesOriginalAccountName),
+					resource.TestCheckResourceAttrSet(anotherResourceTypeAndName, "creation_date"),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "vendor", variable.CloudAccountKubernetesVendor),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "organizational_unit_name", defaultOrganizationalUnitName),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "runtime_protection.0.enabled", strconv.FormatBool(variable.CloudAccountKubernetesRuntimeProtectionEnabled)),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "admission_control.0.enabled", strconv.FormatBool(variable.CloudAccountKubernetesAdmissionControlEnabled)),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "image_assurance.0.enabled", strconv.FormatBool(variable.CloudAccountKubernetesImageAssuranceEnabled)),
+				),
+			},
+			{
+				// Update features
+				Config: testAccCheckCloudAccountKubernetesWithUpdateFeatures(anotherResourceTypeAndName, anotherGeneratedName, variable.CloudAccountKubernetesOriginalAccountName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudAccountKubernetesExists(anotherResourceTypeAndName, &cloudAccountResponse),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "name", variable.CloudAccountKubernetesOriginalAccountName),
+					resource.TestCheckResourceAttrSet(anotherResourceTypeAndName, "creation_date"),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "vendor", variable.CloudAccountKubernetesVendor),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "organizational_unit_name", defaultOrganizationalUnitName),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "runtime_protection.0.enabled", strconv.FormatBool(variable.CloudAccountKubernetesRuntimeProtectionUpdateEnabled)),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "admission_control.0.enabled", strconv.FormatBool(variable.CloudAccountKubernetesAdmissionControlUpdateEnabled)),
+					resource.TestCheckResourceAttr(anotherResourceTypeAndName, "image_assurance.0.enabled", strconv.FormatBool(variable.CloudAccountKubernetesImageAssuranceUpdateEnabled)),
 				),
 			},
 		},
@@ -161,6 +194,50 @@ data "%s" "%s" {
 	)
 }
 
+func testAccCheckCloudAccountKubernetesCreateWithFeatures(resourceTypeAndName, generatedName, resourceName string) string {
+	return fmt.Sprintf(`
+// Kubernetes cloud account with features
+%s
+
+data "%s" "%s" {
+ id = "${%s.id}"
+}
+`,
+		// Kubernetes cloud account
+		getCloudAccountKubernetesResourceHCLWithfeatures(generatedName, resourceName,
+			variable.CloudAccountKubernetesRuntimeProtectionEnabled,
+			variable.CloudAccountKubernetesAdmissionControlEnabled,
+			variable.CloudAccountKubernetesImageAssuranceEnabled),
+
+		// data source variables
+		resourcetype.CloudAccountKubernetes,
+		generatedName,
+		resourceTypeAndName,
+	)
+}
+
+func testAccCheckCloudAccountKubernetesWithUpdateFeatures(resourceTypeAndName, generatedName, resourceName string) string {
+	return fmt.Sprintf(`
+// Kubernetes cloud account with features
+%s
+
+data "%s" "%s" {
+ id = "${%s.id}"
+}
+`,
+		// Kubernetes cloud account
+		getCloudAccountKubernetesResourceHCLWithfeatures(generatedName, resourceName,
+			variable.CloudAccountKubernetesRuntimeProtectionUpdateEnabled,
+			variable.CloudAccountKubernetesAdmissionControlUpdateEnabled,
+			variable.CloudAccountKubernetesImageAssuranceUpdateEnabled),
+
+		// data source variables
+		resourcetype.CloudAccountKubernetes,
+		generatedName,
+		resourceTypeAndName,
+	)
+}
+
 func getBasicCloudAccountKubernetesResourceHCL(generatedName string, resourceName string) string {
 	return fmt.Sprintf(`
 resource "%s" "%s" {
@@ -186,5 +263,30 @@ resource "%s" "%s" {
 		generatedName,
 		resourceName,
 		organizationUnitTypeAndName,
+	)
+}
+
+func getCloudAccountKubernetesResourceHCLWithfeatures(generatedName string, resourceName string, runtimeProtection, admissionControl, imageAssurance bool) string {
+	return fmt.Sprintf(`
+resource "%s" "%s" {
+ name                   = "%s"
+   runtime_protection {
+	enabled = %v
+  }
+  admission_control {
+	enabled = %v
+  }
+  image_assurance {
+	enabled = %v
+  }
+}
+`,
+		// Kubernetes cloud account variables
+		resourcetype.CloudAccountKubernetes,
+		generatedName,
+		resourceName,
+		runtimeProtection,
+		admissionControl,
+		imageAssurance,
 	)
 }
