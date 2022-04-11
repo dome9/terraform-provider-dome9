d421cddbed8a2eee670107ede4631dbe0e4d09f3
MennyAboush <menahema@checkpoint.com>
2022-04-11 20:07:59 +0300

    PLAT-3586-renameFiles

---
+++ dome9/data_source_dome9_aws_unified_onbording_update_version_stack_config.go
@@ -0,0 +1,59 @@
+package dome9
+
+import (
+	"github.com/terraform-providers/terraform-provider-dome9/dome9/common/providerconst"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+
+func dataSourceAwsUnifiedOnboardingUpdateVersionStackConfig() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsUnifiedOnboardingReadConfig,
+		Schema: map[string]*schema.Schema{
+			providerconst.OnboardingId: {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			providerconst.StackName: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			providerconst.Parameters: {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{},
+				},
+			},
+			providerconst.IamCapabilities: {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			providerconst.TemplateUrl: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceAwsUnifiedOnboardingReadConfig(d *schema.ResourceData, meta interface{}) error {
+	d9Client := meta.(*Client)
+	resp, _, err := d9Client.AwsUnifiedOnbording.GetUpdateStackConfig(d.Get(providerconst.OnboardingId).(string))
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Get UnifiedOnbording data resource configuration with stackName: %v\n", resp.StackName)
+
+	d.SetId(d.Get(providerconst.OnboardingId).(string))
+	_ = d.Set(providerconst.StackName, resp.StackName)
+	_ = d.Set(providerconst.Parameters, resp.Parameters)
+	_ = d.Set(providerconst.IamCapabilities, resp.IamCapabilities)
+	_ = d.Set(providerconst.TemplateUrl, resp.TemplateUrl)
+
+	return nil
+}
