db56c3eef337fdb8bf7fe2dab9941bb4bdc3b5a0
MennyAboush <menahema@checkpoint.com>
2022-04-18 17:37:34 +0300

    PLAT-3086-datasource configuration test empty

--- dome9/data_source_dome9_aws_unified_onbording_update_version_stack_config_test.go
+++ dome9/data_source_dome9_aws_unified_onbording_update_version_stack_config_test.go
@@ -1,26 +1,28 @@
 package dome9
 
 import (
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/terraform-providers/terraform-provider-dome9/dome9/common/resourcetype"
 	"github.com/terraform-providers/terraform-provider-dome9/dome9/common/testing/method"
+	"log"
 	"testing"
 )
 
 func TestAccDataSourceAWSUnifiedOnboardingUpdateVersionStackConfogurationBasic(t *testing.T) {
-	resourceTypeAndName, _, resourceName := method.GenerateRandomSourcesTypeAndName(resourcetype.AwsUnifiedOnboarding)
-
+	resourceTypeAndName, dataTypeAndName, resourceName := method.GenerateRandomSourcesTypeAndName(resourcetype.AwsUnifiedOnboarding)
+	log.Println("TestAccDataSourceAWSUnifiedOnboardingUpdateVersionStackConfogurationBasic ",resourceTypeAndName, dataTypeAndName, resourceName)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
 		},
 		Providers:    testAccProviders,
-		//CheckDestroy: testAccCheckAWSUnifiedOnboardingDestroy,
+		CheckDestroy: testAccCheckAWSUnifiedOnboardingDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckAwsUnifiedOnbordingBasic(resourceTypeAndName, resourceName),
+				Config: testAccCheckAwsUnifiedOnbordingUpdateVersionStackConfogurationBasic(resourceTypeAndName, resourceName),
 				Check: resource.ComposeTestCheckFunc(
-					//resource.TestCheckResourceAttrPair(resourceTypeAndName+"Data", "cloud_vendor", resourceTypeAndName, "cloud_vendor"),
+					//resource.TestCheckResourceAttrPair(dataName+"Data", "ID", resourceTypeAndName, "ID"),
 					//resource.TestCheckResourceAttrPair(dataSourceTypeAndName, "cloud_vendor", resourceTypeAndName, "cloud_vendor"),
 					//resource.TestCheckResourceAttrPair(dataSourceTypeAndName, "enable_stack_modify", resourceTypeAndName, "enable_stack_modify"),
 					//resource.TestCheckResourceAttrPair(dataSourceTypeAndName, "full_protection", resourceTypeAndName, "full_protection"),
@@ -28,4 +30,31 @@ func TestAccDataSourceAWSUnifiedOnboardingUpdateVersionStackConfogurationBasic(t
 			},
 		},
 	})
+}
+
+func testAccCheckAwsUnifiedOnbordingUpdateVersionStackConfogurationBasic(resourceTypeAndName string, generatedName string) string {
+	res := fmt.Sprintf(`
+// AwsUnifiedOnbording resource
+
+%s
+
+data "%s" "%s" {
+  onboarding_id = "${%s.id}"
+}
+`,
+		// continuous compliance notification resource
+		getAwsUnifiedOnboardingHCL(generatedName),
+
+		// data source variables
+		resourcetype.AwsUnifiedOnboardingUpdateVersionStackConfig,
+		generatedName+"Data",
+		resourceTypeAndName,
+	)
+	log.Printf("[INFO] testAccCheckAwsUnifiedOnbordingBasic:%+v\n", res)
+
+	return res
+}
+
+func GenerateAWSUnifiedOnboardingUpdateVersionStackConfogurationName(name string) interface{} {
+	return fmt.Sprintf("data.%s.%s", resourcetype.AwsUnifiedOnboardingUpdateVersionStackConfig, name)
 }
