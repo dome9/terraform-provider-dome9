2f72857df2b0084e8a4a84abc5e50b79b8dcec18
omark <omark@checkpoint.com>
2022-06-22 09:03:47 +0300

    Relocate Common File Location to make private access to functions

--- dome9/common_utils.go
+++ dome9/common_utils.go
@@ -2,7 +2,7 @@ package dome9
 
 import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
-func ExpandNotificationIDs(d *schema.ResourceData, key string) []string {
+func expandNotificationIDs(d *schema.ResourceData, key string) []string {
 	notificationsIDsData := d.Get(key).([]interface{})
 	notificationIDsList := make([]string, len(notificationsIDsData))
 	for i, notificationID := range notificationsIDsData {
--- dome9/resource_dome9_continuous_compliance_policy.go
+++ dome9/resource_dome9_continuous_compliance_policy.go
@@ -7,7 +7,6 @@ import (
 	"github.com/dome9/dome9-sdk-go/services/compliance/continuous_compliance_policy"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-	"github.com/terraform-providers/terraform-provider-dome9/dome9/common"
 )
 
 func resourceContinuousCompliancePolicy() *schema.Resource {
@@ -110,7 +109,7 @@ func expandContinuousCompliancePolicyRequest(d *schema.ResourceData) continuous_
 	return continuous_compliance_policy.ContinuousCompliancePolicyRequest{
 		TargetId:        d.Get("target_id").(string),
 		RulesetId:       d.Get("ruleset_id").(int),
-		NotificationIds: dome9.ExpandNotificationIDs(d, "notification_ids"),
+		NotificationIds: expandNotificationIDs(d, "notification_ids"),
 		TargetType:      d.Get("target_type").(string),
 	}
 }
