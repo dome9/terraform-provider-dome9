e73acf705f100b75e2d43afc907d18b204ea89e1
Dolev <dolevh@checkpoint.com>
2022-07-26 16:11:30 +0300

    WIP

--- dome9/resource_dome9_assessment.go
+++ dome9/resource_dome9_assessment.go
@@ -1,12 +1,12 @@
 package dome9
 
 import (
+	"github.com/dome9/dome9-sdk-go/dome9/client"
+	"github.com/dome9/dome9-sdk-go/services/assessment"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-dome9/dome9/common/providerconst"
 	"log"
-
-	"github.com/dome9/dome9-sdk-go/dome9/client"
 )
 
 func resourceAssessment() *schema.Resource {
@@ -20,7 +20,7 @@ func resourceAssessment() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-			"id": {
+			"bundle_id": {
 				Type:     schema.TypeInt,
 				Required: true,
 			},
@@ -28,16 +28,16 @@ func resourceAssessment() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"cloudAccountId": {
+			"cloud_account_id": {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"cloudAccountType": {
-				Type:     schema.TypeString,
-				Required: true,
+			"cloud_account_type": {
+				Type:         schema.TypeString,
+				Required:     true,
 				ValidateFunc: validation.StringInSlice(providerconst.AssessmentCloudAccountType, false),
 			},
-			"shouldMinimizeResult": {
+			"should_minimize_result": {
 				Type:     schema.TypeBool,
 				Required: true,
 			},
@@ -310,49 +310,61 @@ func resourceAssessment() *schema.Resource {
 
 func resourceAssessmentCreate(d *schema.ResourceData, meta interface{}) error {
 	d9Client := meta.(*Client)
-	req := expandCloudAccountAlibabaRequest(d)
-	log.Printf("[INFO] Creating Alibaba Cloud Account with request %+v\n", req)
+	req := expandAssessmentRequest(d)
+	log.Printf("[INFO] Creating assessment with request %+v\n", req)
 
-	resp, _, err := d9Client.cloudaccountAlibaba.Create(req)
+	resp, _, err := d9Client.assessment.Run(&req)
 	if err != nil {
 		return err
 	}
 
-	log.Printf("[INFO] Created Alibaba CloudAccount. ID: %v\n", resp.ID)
-	d.SetId(resp.ID)
+	log.Printf("[INFO] Created assessment. ID: %v\n", resp.ID)
+	d.SetId(string(rune(resp.ID)))
 
 	return resourceAssessmentRead(d, meta)
 }
 
 func resourceAssessmentRead(d *schema.ResourceData, meta interface{}) error {
 	d9Client := meta.(*Client)
-	resp, _, err := d9Client.assessment.RunBundle(d.Id())
+	resp, _, err := d9Client.assessment.Get(d.Id())
 
 	if err != nil {
 		if err.(*client.ErrorResponse).IsObjectNotFound() {
-			log.Printf("[WARN] Removing Alibaba cloud account %s from state because it no longer exists in Dome9", d.Id())
+			log.Printf("[WARN] Removing assessment %s from state because it no longer exists in Dome9", d.Id())
 			d.SetId("")
 			return nil
 		}
-
 		return err
 	}
 
-	d.SetId(resp.ID)
-	_ = d.Set("name", resp.Name)
-	_ = d.Set("alibaba_account_id", resp.AlibabaAccountId)
-	_ = d.Set("vendor", resp.Vendor)
-	_ = d.Set("creation_date", resp.CreationDate)
-	_ = d.Set("organizational_unit_id", resp.OrganizationalUnitID)
-	_ = d.Set("organizational_unit_path", resp.OrganizationalUnitPath)
-	_ = d.Set("organizational_unit_name", resp.OrganizationalUnitName)
+	d.SetId(string(rune(resp.ID)))
+	_ = d.Set("test_entities", resp.TestEntities)
+	_ = d.Set("assessment_passed", resp.AssessmentPassed)
+	_ = d.Set("has_errors", resp.HasErrors)
+	_ = d.Set("assessment_id", resp.AssessmentId)
+
+	if err := d.Set("request", flattenAssessmentRequest(resp.Request)); err != nil {
+		return err
+	}
+
+	if err := d.Set("tests", flattenAssessmentTests(resp.Tests)); err != nil {
+		return err
+	}
+
+	if err := d.Set("location_metadata", flattenAssessmentLocationMetadata(resp.LocationMetadata)); err != nil {
+		return err
+	}
+
+	if err := d.Set("data_sync_status", flattenAssessmentDataSyncStatus(resp.DataSyncStatus)); err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func resourceAssessmentDelete(d *schema.ResourceData, meta interface{}) error {
 	d9Client := meta.(*Client)
-	log.Printf("[INFO] Deleting Alibaba Cloud Account ID: %v\n", d.Id())
+	log.Printf("[INFO] Deleting assessment ID: %v\n", d.Id())
 	if _, err := d9Client.cloudaccountAlibaba.Delete(d.Id()); err != nil {
 		return err
 	}
@@ -361,7 +373,173 @@ func resourceAssessmentDelete(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceAssessmentUpdate(d *schema.ResourceData, meta interface{}) error {
-	log.Println("An update can not be made to an assessment")
+	log.Println("[WARN] An update can not be made to an assessment")
 	return nil
 }
 
+func expandAssessmentRequest(d *schema.ResourceData) assessment.RunBundleRequest {
+	req := assessment.RunBundleRequest{
+		BundleID:               d.Get("bundle_id").(int),
+		Dome9CloudAccountID:    d.Get("dome9_cloud_account_id").(string),
+		CloudAccountID:         d.Get("cloud_account_id").(string),
+		CloudAccountType:       d.Get("cloud_account_type").(string),
+		ShouldMinimizeResult:   d.Get("should_minimize_result").(bool),
+		Name:                   d.Get("name").(string),
+		Description:            d.Get("description").(string),
+		ExternalCloudAccountID: d.Get("external_cloud_account_id").(string),
+		RequestID:              d.Get("request_id").(string),
+	}
+	return req
+}
+
+func flattenAssessmentRequest(Request assessment.Request) []interface{} {
+	m := map[string]interface{}{
+		"dome9_cloud_account_id":    Request.Dome9CloudAccountID,
+		"external_cloud_account_id": Request.ExternalCloudAccountID,
+		"cloud_account_id":          Request.CloudAccountID,
+		"cloud_account_type":        Request.CloudAccountType,
+		"request_id":                Request.RequestID,
+		"should_minimize_result":    Request.ShouldMinimizeResult,
+	}
+
+	return []interface{}{m}
+}
+
+func flattenAssessmentTests(tests []assessment.Test) []interface{} {
+	allTests := make([]interface{}, len(tests))
+	for i, val := range tests {
+		allTests[i] = map[string]interface{}{
+			"error":               val.Error,
+			"tested_count":        val.TestedCount,
+			"relevant_count":      val.RelevantCount,
+			"non_complying_count": val.NonComplyingCount,
+			"exclusion_stats":     flattenAssessmentTestsExclusionStats(val.ExclusionStats),
+			"entity_results":      flattenAssessmentTestsEntityResults(val.EntityResults),
+			"rule":                flattenAssessmentTestsRule(val.Rule),
+			"test_passed":         val.TestPassed,
+		}
+	}
+
+	return allTests
+}
+
+func flattenAssessmentTestsExclusionStats(Request assessment.ExclusionStats) []interface{} {
+	m := map[string]interface{}{
+		"tested_count":        Request.TestedCount,
+		"relevant_count":      Request.RelevantCount,
+		"non_complying_count": Request.NonComplyingCount,
+	}
+
+	return []interface{}{m}
+}
+
+func flattenAssessmentTestsEntityResults(entityResults []assessment.EntityResult) []interface{} {
+	allEntityResults := make([]interface{}, len(entityResults))
+	for i, val := range entityResults {
+		allEntityResults[i] = map[string]interface{}{
+			"validation_status": val.ValidationStatus,
+			"is_relevant":       val.IsRelevant,
+			"is_valid":          val.IsValid,
+			"is_excluded":       val.IsExcluded,
+			"exclusion_id":      val.ExclusionID,
+			"remediation_id":    val.RemediationID,
+			"error":             val.Error,
+			"test_obj":          val.TestObj, //TODO: can cause a problem: interface{}
+		}
+	}
+
+	return allEntityResults
+}
+
+func flattenAssessmentTestsRule(Request assessment.Rule) []interface{} {
+	m := map[string]interface{}{
+		"name":          Request.Name,
+		"severity":      Request.Severity,
+		"logic":         Request.Logic,
+		"description":   Request.Description,
+		"remediation":   Request.Remediation,
+		"cloudbots":     Request.Cloudbots,
+		"complianceTag": Request.ComplianceTag,
+		"domain":        Request.Domain,
+		"priority":      Request.Priority,
+		"controlTitle":  Request.ControlTitle,
+		"ruleId":        Request.RuleID,
+		"category":      Request.Category,
+		"labels":        Request.Labels, //TODO: can cause a problem: []string
+		"logic_hash":    Request.LogicHash,
+		"is_default":    Request.IsDefault,
+	}
+
+	return []interface{}{m}
+}
+
+func flattenAssessmentLocationMetadata(locationMetadata assessment.LocationMetadata) []interface{} {
+	m := map[string]interface{}{
+		"account": flattenAssessmentLocationMetadataAccount(locationMetadata.Account),
+	}
+
+	return []interface{}{m}
+}
+
+func flattenAssessmentLocationMetadataAccount(Request assessment.Account) []interface{} {
+	m := map[string]interface{}{
+		"srl":        Request.Srl,
+		"name":       Request.Name,
+		"id":         Request.ID,
+		"externalId": Request.ExternalID,
+	}
+
+	return []interface{}{m}
+}
+
+func flattenAssessmentDataSyncStatus(dataSyncStatus []assessment.DataSyncStatus) []interface{} {
+	allDataSyncStatus := make([]interface{}, len(dataSyncStatus))
+	for i, val := range dataSyncStatus {
+		allDataSyncStatus[i] = map[string]interface{}{
+			"entity_type":                     val.EntityType,
+			"recently_successful_sync":        val.RecentlySuccessfulSync,
+			"general_fetch_permission_issues": val.GeneralFetchPermissionIssues,
+			"entities_with_permission_issues": flattenAssessmentDataSyncStatusEntitiesWithPermissionIssues(val.EntitiesWithPermissionIssues),
+		}
+	}
+
+	return allDataSyncStatus
+}
+
+func flattenAssessmentDataSyncStatusEntitiesWithPermissionIssues(entitiesWithPermissionIssues []assessment.EntitiesWithPermissionIssues) []interface{} {
+	allTests := make([]interface{}, len(entitiesWithPermissionIssues))
+	for i, val := range entitiesWithPermissionIssues {
+		allTests[i] = map[string]interface{}{
+			"external_id":             val.ExternalID,
+			"name":                    val.Name,
+			"cloud_vendor_identifier": val.CloudVendorIdentifier,
+		}
+	}
+
+	return allTests
+}
+
+/*
+func flattenAssessment(Request assessment.Request) []interface{} {
+	m := map[string]interface{}{
+		"":    Request.,
+
+	}
+
+	return []interface{}{m}
+}
+
+func flattenAssessment(Tests []assessment.Test) []interface{} {
+	allTests := make([]interface{}, len(Tests))
+	for i, val := range Tests {
+		allTests[i] = map[string]interface{}{
+			"":  val.,
+
+		}
+	}
+
+	return allTests
+}
+
+
+*/
