900170de38961a574f8bf0372b8dfdece11b8fa2
Yogev Ktuvim <yogev@dome9.com>
2025-01-23 16:42:17 +0200

    CNAPP-132259-Convert-NotificationIds-To-Set

--- dome9/common_utils.go
+++ dome9/common_utils.go
@@ -3,10 +3,19 @@ package dome9
 import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
 func expandNotificationIDs(d *schema.ResourceData, key string) []string {
-	notificationsIDsData := d.Get(key).(*schema.Set).List()
-	notificationIDsList := make([]string, len(notificationsIDsData))
-	for i, notificationID := range notificationsIDsData {
-		notificationIDsList[i] = notificationID.(string)
+	rawData := d.Get(key).([]interface{}) // Take the list as-is
+	uniqueSet := make(map[string]struct{})
+
+	// Convert the list to a set (map) to ensure uniqueness
+	for _, id := range rawData {
+		uniqueSet[id.(string)] = struct{}{}
 	}
+
+	// Convert the set back to a slice
+	notificationIDsList := make([]string, 0, len(uniqueSet))
+	for id := range uniqueSet {
+		notificationIDsList = append(notificationIDsList, id)
+	}
+
 	return notificationIDsList
 }
